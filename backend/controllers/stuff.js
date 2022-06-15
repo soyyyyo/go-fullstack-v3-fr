395f3ef46d6173758fa4f59c0cfdb32739d7ac79
soyyyyo <94390996+soyyyyo@users.noreply.github.com>
2022-06-15 14:56:46 +0200

    courses done

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing);
@@ -29,6 +30,18 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
+  const thingObject = req.file ?
+    {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+/*
+exports.modifyThing = (req, res, next) => {
   const thing = new Thing({
     _id: req.params.id,
     title: req.body.title,
@@ -51,7 +64,22 @@ exports.modifyThing = (req, res, next) => {
     }
   );
 };
+*/
 
+exports.deleteThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => {
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Thing.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+/*
 exports.deleteThing = (req, res, next) => {
   Thing.findOne({ _id: req.params.id }).then(
     (thing) => {
@@ -81,6 +109,7 @@ exports.deleteThing = (req, res, next) => {
     }
   )
 };
+*/
 
 
 /*
